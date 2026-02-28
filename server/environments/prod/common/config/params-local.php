e1f1d20d6578d5bb0bfefeab550ae451349eaa16
ecex <zeqweras@gmail.com>
2026-02-28 10:11:01 +0800

    fix: 更新params-local.php生产配置，替换内网地址为ecex.cc

--- server/environments/prod/common/config/params-local.php
+++ server/environments/prod/common/config/params-local.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'redis.prefix' => 'dev-stock',
-    'cdn_url' => 'http://192.168.30.119:8199',
-    'base_url' => 'http://192.168.30.119:8199',
-    'return_url' => 'http://192.168.30.119:8199',
-    'return_url_app' => 'http://192.168.30.119:8199'
+    'redis.prefix' => 'prod-ecex',
+    'cdn_url' => 'https://ecex.cc/api',
+    'base_url' => 'https://ecex.cc/api',
+    'return_url' => 'https://ecex.cc',
+    'return_url_app' => 'https://ecex.cc'
 ];
