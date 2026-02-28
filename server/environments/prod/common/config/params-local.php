ead36186d424cff12126b69589d03aacbbdf086f
ecex <zeqweras@gmail.com>
2026-02-28 10:19:45 +0800

    fix: 添加resourceHostCdn和admin_cdn_url参数解决图片URL拼接报错

--- server/environments/prod/common/config/params-local.php
+++ server/environments/prod/common/config/params-local.php
@@ -5,5 +5,7 @@ return [
     'cdn_url' => 'https://ecex.cc/api',
     'base_url' => 'https://ecex.cc/api',
     'return_url' => 'https://ecex.cc',
-    'return_url_app' => 'https://ecex.cc'
+    'return_url_app' => 'https://ecex.cc',
+    'resourceHostCdn' => 'https://ecex.cc/api',
+    'admin_cdn_url' => 'https://ecex.cc/api',
 ];
